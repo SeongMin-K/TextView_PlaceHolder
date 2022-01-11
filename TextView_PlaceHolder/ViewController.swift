6740439b211fe9eeeafa0297f5b727757160aa44
SeongMin-K <eo1tks3@naver.com>
2022-01-11 15:31:22 +0900

    TextView PlaceHolder

--- TextView_PlaceHolder/ViewController.swift
+++ TextView_PlaceHolder/ViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import UITextView_Placeholder
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myTextView.placeholder = "This is custom placeholder!"
+        myTextView.placeholderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 
 
